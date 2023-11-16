7ec6effb80d15f9501e517ec8bd3e6beb8494f46
Ying <ge84xof@mytum.de>
2023-11-16 16:42:46 +0100

    interactive between box plot and bar chart

--- new_combineation_multiple_choices.py
+++ new_combineation_multiple_choices.py
@@ -39,18 +39,7 @@ clusters = sorted(df['cluster'].unique())
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Graph(id='bar-chart', figure={
-        'data': [
-            {'x': normalized_avg_df.index, 'y': normalized_avg_df['UTCI'], 'type': 'bar', 'name': 'UTCI'},
-            {'x': normalized_avg_df.index, 'y': normalized_avg_df['GWP'], 'type': 'bar', 'name': 'GWP'},
-            {'x': normalized_avg_df.index, 'y': normalized_avg_df['LCC'], 'type': 'bar', 'name': 'LCC'}
-        ],
-        'layout': {
-            'title': 'Normalized Cluster Averages',
-            'xaxis': {'title': 'Cluster'},
-            'yaxis': {'title': 'Normalized Value'}
-        }
-    }),
+    dcc.Graph(id='bar-chart'),
     dcc.Dropdown(
         id='cluster-dropdown',
         options=[{'label': f"Cluster {cluster}", 'value': cluster} for cluster in clusters],
@@ -63,25 +52,63 @@ app.layout = html.Div([
 
 
 @app.callback(
+    Output('bar-chart', 'figure'),
+    [Input('cluster-dropdown', 'value')]
+)
+def update_bar_chart(selected_clusters):
+    bar_data = []
+
+    # 为每个指标分配一个颜色
+    indicator_colors = {
+        'UTCI': 'red',
+        'GWP': 'green',
+        'LCC': 'blue'
+    }
+
+    legend_added = {indicator: False for indicator in indicators}
+
+    for cluster in normalized_avg_df.index:
+        for indicator in indicators:
+            # 当前指标的默认颜色
+            show_legend = not legend_added[indicator]
+            color = indicator_colors[indicator] if cluster in selected_clusters else 'rgba(204, 204, 204, 0.7)'
+            bar_data.append(
+                go.Bar(
+                    x=[f"Cluster{cluster}"],
+                    y=[normalized_avg_df.loc[cluster, indicator]],
+                    name=indicator,
+                    marker_color=color,
+                    showlegend=show_legend
+                )
+            )
+            legend_added[indicator] = True
+
+    return {
+        'data': bar_data,
+        'layout': {
+            'title': 'Normalized Cluster Averages',
+            'xaxis': {'title': 'Cluster'},
+            'yaxis': {
+                'title': 'Ereignis der Aspekte',
+                'tickvals': [0, 0.5, 1],
+                'ticktext': ['Gut', 'Mittel', 'Schlecht']
+            },
+            'barmode': 'group',
+            'bargap': 0.000001,
+            'bargroupgap': 0.001,
+
+        }
+    }
+
+
+@app.callback(
     Output('box-plots-container', 'children'),
     [Input('cluster-dropdown', 'value')]
 )
 def update_box_plots(selected_clusters):
-    # 合并选定的所有聚类数据
     filtered_df = df_normalized[df['cluster'].isin(selected_clusters)]
-
-    # 为每个参数创建盒形图
-    traces = []
-    for parameter in parameters:
-        traces.append(
-            go.Box(
-                y=filtered_df[parameter],
-                name=parameter,
-                boxpoints='all',  # 显示所有数据点
-                jitter=0.3,
-                pointpos=-1.8
-            )
-        )
+    traces = [go.Box(y=filtered_df[parameter], name=parameter, boxpoints='all', jitter=0.3, pointpos=-1.8) for parameter
+              in parameters]
 
     return dcc.Graph(
         figure={
